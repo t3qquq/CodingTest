d95589a23485738fb25d9216a79ff7cc7f0bc3c4
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-03-22 10:43:37 +0900

    [level 0] Title: 개미 군단, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        answer+=hp/5;
+        answer+=hp%5/3;
+        answer+=hp%5%3;
+        
+        return answer;
+    }
+}
