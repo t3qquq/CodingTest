cc24011569c06cdfe71f82f2f61104e166670b5d
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-03-22 10:19:13 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.36 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, string letter) {
+        string answer = "";
+        
+        for(int i=0; i<my_string.Length; i++){
+            if(my_string[i]!=(letter[0]))
+                answer+=my_string[i];
+        }
+        
+        return answer;
+    }
+}
