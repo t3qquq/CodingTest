cadf209f0f9bd4e2d0de568e30d99a3494173f9b
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-03-22 10:34:13 +0900

    [level 0] Title: 편지, Time: 0.14 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(string message) {
+        int answer = 0;
+        
+        answer=message.Length*2;
+        
+        return answer;
+    }
+}
