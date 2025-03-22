a8206149c931a41558a84d89be75b1a116b73b63
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-03-22 10:31:39 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 1.55 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        Array.Sort(numbers);
+        
+        answer = numbers[numbers.Length-1]*numbers[numbers.Length-2];
+        
+        return answer;
+    }
+}
