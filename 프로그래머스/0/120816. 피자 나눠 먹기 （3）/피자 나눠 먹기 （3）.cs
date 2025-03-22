3810584abe42c5832e2fd0d07999e4b500cbc9c0
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-03-22 10:09:51 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.19 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        
+        while(true){
+            if((slice*answer)/n>0){
+                break;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
