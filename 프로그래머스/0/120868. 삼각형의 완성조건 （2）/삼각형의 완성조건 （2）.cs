36644180396037452683313661540bc5f3d6bb07
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-03-26 22:37:43 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 2.13 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;        
+        
+        Array.Sort(sides);
+        
+        for(int i=sides[1]-sides[0]+1; i<sides[0]+sides[1]; i++){
+            answer++;
+        }
+        
+        return answer;
+    }
+}
