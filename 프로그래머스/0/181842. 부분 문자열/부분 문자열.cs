47ea37aaf862f112b54f0b35c77a61ef9250ccb7
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-04-07 19:02:57 +0900

    [level 0] Title: 부분 문자열, Time: 0.33 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(string str1, string str2) {
+        int answer = 0;
+        
+        for(int i=0; i<str2.Length-str1.Length+1; i++){
+            if(str2.Substring(i,str1.Length)==str1){
+                answer=1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
