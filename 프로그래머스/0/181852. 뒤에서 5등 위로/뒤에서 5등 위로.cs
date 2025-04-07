b589b679645adb67ee5c02e8f9641483e78b7bf5
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-04-07 19:27:01 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 2.50 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.Length-5];
+        
+        Array.Sort(num_list);
+        Array.Copy(num_list, 5, answer, 0, num_list.Length-5);
+        
+        return answer;
+    }
+}
