b3bb2532642f8a6782b1c13fd51eaf453049a8ac
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-04-09 15:26:39 +0900

    [level 0] Title: 5명씩, Time: 0.24 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string[] solution(string[] names) {
+        string[] answer = new string[(names.Length-1)/5+1];
+        for(int i=0; i<answer.Length; i++){
+            answer[i]=names[5*i];
+        }
+        return answer;
+    }
+}
