38fe7af7db9c0cb99a2bd2944cd35d7aff46db3e
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-04-09 16:11:01 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.28 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = -1;
+        
+        for(int i=idx; i<arr.Length; i++){
+            if(arr[i]==1){
+                answer=i;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
