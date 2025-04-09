d75137eb2983bb8f23ccc93eeff701c27835efb5
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-04-09 15:05:08 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.29 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        
+        for(int i=0; i<numbers.Length; i++){
+            answer+=numbers[i];
+            if(answer>n)
+                break;
+        }
+        
+        return answer;
+    }
+}
