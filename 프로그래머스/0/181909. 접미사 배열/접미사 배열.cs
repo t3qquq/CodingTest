083d2bc02172eca59391b80a686c593d08cf1e5c
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-04-11 17:23:25 +0900

    [level 0] Title: 접미사 배열, Time: 5.75 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public string[] solution(string my_string) {
+        string[] answer = new string[my_string.Length];
+        for(int i=0; i<my_string.Length; i++){
+            answer[i]=my_string.Substring(i);
+        }
+        Array.Sort(answer);
+        return answer;
+    }
+}
