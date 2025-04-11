5314ac81420596e172b5eafcda71b3a6affa6c06
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-04-11 22:12:20 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.29 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c) {
+        
+        return a==b&&b==c ? (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c) : a==b||b==c||a==c ? (a+b+c)*(a*a+b*b+c*c) : a+b+c;
+    }
+}
