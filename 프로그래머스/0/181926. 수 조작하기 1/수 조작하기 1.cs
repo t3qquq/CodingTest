4c2bfc02c32dafae34f8c278b707402251d2c6b3
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-04-12 16:04:43 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.31 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Solution {
+    public int solution(int n, string control) {
+
+        for(int i=0; i<control.Length; i++){
+            switch(control[i]){
+                case 'w':
+                    n++;
+                    break;
+                    
+                case 's':
+                    n--;
+                    break;
+                    
+                case 'd':
+                    n+=10;
+                    break;
+                    
+                case 'a':
+                    n-=10;
+                    break;
+            }
+        }
+        
+        return n;
+    }
+}
